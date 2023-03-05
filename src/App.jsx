29e76674a28825050e88127deeaba7cf21fed103
omkar-deshmukh1804 <omideshmukh11@gmail.com>
2023-03-05 22:09:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="main--container">
+      <div className='card--container'>
+        <div className='qr--container'>
+          <img src='./public/image-qr-code.png' className='qr--image' />
+        </div>
+
+        <div className='text--conatiner'>
+          <h3>Improve your front-end skills by building projects</h3>
+          <h6>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h6>
+        </div>
+
+      </div>
+    </div> 
+  )
+  
+}
+
+export default App
